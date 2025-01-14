b1db3b1201c772f73059e0651c57264bab8dc40b
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-01-14 18:35:09 +0600

    form added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <div className="text-3xl">Register</div>
+            <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col ">
+    <div className="text-center ">
+      <h1 className="text-5xl font-bold">Register now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Name</span>
+          </label>
+          <input type="text" placeholder="Your Name" name='name'  className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email'  className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+      <p>User Of this site?Please <Link to='/login' ><button className='btn btn-link'>Log in</button></Link> </p>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
